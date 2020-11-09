711bbbf665a725ee2004544d19624801059e907b
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-11-09 07:29:16 +0700

    done

--- app/src/main/java/com/example/fundamentalsimplenotif/MainActivity.kt
+++ app/src/main/java/com/example/fundamentalsimplenotif/MainActivity.kt
@@ -1,31 +1,50 @@
 package com.example.fundamentalsimplenotif
 
+import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.BitmapFactory
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 
 class MainActivity : AppCompatActivity() {
     companion object {
         const val NOTIFICATION_ID = 1
         var CHANNEL_ID = "channel_01"
-        var CHANNEL_NAME: CharSequence = "dicoding channel"
+        var CHANNEL_NAME = "dicoding channel"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sendNotification(view: View) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.arifikhsanudin.my.id"))
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
 
         val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentIntent(pendingIntent)
                 .setSmallIcon(R.drawable.ic_notifications_white_48px)
                 .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_notifications_white_48px))
                 .setContentTitle(resources.getString(R.string.content_title))
                 .setContentText(resources.getString(R.string.content_text))
                 .setSubText(resources.getString(R.string.subtext))
                 .setAutoCancel(true)
+
+        val channel = NotificationChannel(CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT)
+        channel.description = CHANNEL_NAME
+        mBuilder.setChannelId(CHANNEL_ID)
+        mNotificationManager.createNotificationChannel(channel)
+
         val notification = mBuilder.build()
         mNotificationManager.notify(NOTIFICATION_ID, notification)
     }
